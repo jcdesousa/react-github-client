4989569d4d3b63bc8342b0af5f2929ec07092099
José Sousa <jose.sousa@xpand-it.com>
2018-08-30 00:28:19 +0100

    feat: create component for top contributors title

---
+++ src/components/TopContributorsTitle.jsx
@@ -0,0 +1,35 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+class TopContributorsTitle extends PureComponent {
+    static propTypes = {
+      repo: PropTypes.string.isRequired,
+    };
+
+    render() {
+      const { repo } = this.props;
+
+      return (
+        <PageTitle>
+          <h3>
+            <span>Top 10 Contributors for </span>
+            <PageRepo>{repo}</PageRepo>
+          </h3>
+        </PageTitle>
+      );
+    }
+}
+
+export default TopContributorsTitle;
+
+const PageTitle = styled.div`
+  font-size: 20px;
+  text-align: center;
+  padding-bottom:20px;
+`;
+
+const PageRepo = styled.span`
+  text-transform: capitalize;
+  font-weight: 500;
+`;
